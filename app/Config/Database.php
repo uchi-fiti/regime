5b819b2e637fbc2de187d10abf64c2fe6ddaaaa8
anjara <loranasl17@gmail.com>
2026-05-08 13:45:40 +0300

    mettre le nom de la db en config

--- app/Config/Database.php
+++ app/Config/Database.php
@@ -29,7 +29,7 @@ class Database extends Config
         'hostname'     => 'localhost',
         'username'     => '',
         'password'     => '',
-        'database'     => '',
+        'database'     => 'regime_db',
         'DBDriver'     => 'MySQLi',
         'DBPrefix'     => '',
         'pConnect'     => false,
