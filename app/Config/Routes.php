37a5415c0f9937627a5cd92bf7ea2fdb55da7eae
Osamu95 <jonathanandriamalaza@gmail.com>
2026-05-08 22:45:33 +0300

    routage pages et refact css

--- app/Config/Routes.php
+++ app/Config/Routes.php
@@ -6,3 +6,12 @@ use CodeIgniter\Router\RouteCollection;
  * @var RouteCollection $routes
  */
 $routes->get('/', 'Home::index');
+
+// Routes pour les pages d'authentification et inscription
+$routes->get('/inscription', 'Auth::inscription');
+$routes->get('/connection', 'Auth::connection');
+$routes->get('/information', 'Auth::information');
+$routes->get('/choose-obj', 'Auth::chooseObj');
+$routes->get('/model', 'Page::model');
+
+
---
+++ app/Controllers/Page.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace App\Controllers;
+
+class Page extends BaseController
+{
+    /**
+     * Affiche la page model/template
+     */
+    public function model()
+    {
+        return view('model');
+    }
+}
